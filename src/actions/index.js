1a5e7c5667b46ce131eb2caa55aa09f19a38e2fb
Ghevond <ghevondtunyan@gemail.com>
2021-04-24 18:45:15 +0400

    resolve merge conflicts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,22 @@ const menuLoaded = (newMenu) => {
     payload: newMenu,
   };
 };
+const menuRequested = () => {
+  return {
+    type: "MENUE_REQUESTED",
+  };
+};
+const addedToCart = (id) => {
+  return {
+    type: "ITEM_ADD_TO_CART",
+    payload: id,
+  };
+};
+const deleteFromCart = (id) => {
+  return {
+    type: "ITEM_DELETE_TO_CART",
+    payload: id,
+  };
+};
 
-export { menuLoaded };
+export { menuLoaded, menuRequested, addedToCart, deleteFromCart };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,37 @@ const reducer = (state = initialState, action) => {
     case "MENUE_LOADED":
       return {
         menu: action.payload,
+        loading: false,
+      };
+    case "MENUE_REQUESTED":
+      return {
+        ...state,
+        menu: state.menu,
+        loading: true,
+      };
+    case "ITEM_ADD_TO_CART":
+      const id = action.payload;
+      const item = state.menu.find((item) => item.id === id);
+
+      const newItem = {
+        title: item.title,
+        price: item.price,
+        url: item.url,
+        id: item.id,
+      };
+      return {
+        ...state,
+        items: [...state.items, newItem],
+      };
+    case "ITEM_DELETE_TO_CART":
+      const idx = action.payload;
+      const itemIndex = state.items.findIndex((item) => item.id === idx);
+      return {
+        ...state,
+        items: [
+          ...state.items.slice(0, itemIndex),
+          ...state.items.slice(itemIndex + 1),
+        ],
       };
     default:
       return state;
